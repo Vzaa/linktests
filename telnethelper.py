79022a6602903fe375afa83a02dce40204d0668c
Vzaa <Vzaa@users.noreply.github.com>
2014-12-19 13:47:54 +0200

    progress
    Issue:

--- telnethelper.py
+++ telnethelper.py
@@ -1,24 +1,28 @@
 "telnet helper with pexpect"
-from __future__ import unicode_literals
+#from __future__ import unicode_literals
 import pexpect
 
 
-def do_remote(hostname, command):
-    c = pexpect.spawnu('telnet ' + hostname)
+def do_remote(hostname, command, username='root', passwd=''):
+    c = pexpect.spawn('telnet ' + hostname)
     c.expect('login:')
-    c.sendline('root')
+    c.sendline(username)
     c.expect('Password:')
-    c.sendline('')
+    c.sendline(passwd)
     c.expect('#')
     c.sendline(command)
     c.expect('#')
+    res = c.before
     c.sendline('exit')
     c.kill(1)
-    pass
+    return res.split('\n')
+
+
 
 def main():
-    do_remote('192.168.2.5', "wl ssid kedi")
-    pass
+    #do_remote('192.168.2.5', "wl ssid kedi")
+    #get_output('192.168.2.5', "ifconfig wl0 | grep -o ..:..:..:..:..:..")
+    asd = do_remote('192.168.2.5', "ls -l")
 
 if __name__ == '__main__':
     main()
--- testnode.py
+++ testnode.py
@@ -1,6 +1,11 @@
 "AP node class"
 
-from telnethelper import do_remote
+from telnethelper import do_remote, get_output
+
+
+def wl_cmd(ifname, command):
+    cmd = 'wl -i ' + ifname + ' ' + command + ';'
+    return cmd
 
 
 class ApNode(object):
@@ -9,64 +14,67 @@ class ApNode(object):
         self.hostname = hostname
         self.switchport = switchport
         self.plchostname = plchostname
-        self.if2g = if2g
-        self.if5g = if5g
+        self.ifs = {2: if2g, 5: if5g}
         self.radio_enabled = True
+        self.macs = {2: self.learn_mac(2), 5: self.learn_mac(5)}
 
     def run_command(self, command):
-        do_remote(self.hostname, command)
-
-    def band_2_ifname(self, band):
-        ifname = None
-        if band == 2:
-            ifname = self.if2g
-        else:
-            ifname = self.if5g
-        return ifname
+        return do_remote(self.hostname, command)
 
     def ap_mode(self, band=5):
-        ifname = self.band_2_ifname(band)
+        ifname = self.ifs[band]
         cmd = ''
-        cmd += 'wl -i ' + ifname + ' down;'
-        cmd += 'wl -i ' + ifname + ' ap 1;'
-        cmd += 'wl -i ' + ifname + ' up;'
+        cmd += wl_cmd(ifname, 'down')
+        cmd += wl_cmd(ifname, 'ap 1')
+        cmd += wl_cmd(ifname, 'up')
         self.run_command(cmd)
 
     def sta_mode(self, band=5):
-        ifname = self.band_2_ifname(band)
+        ifname = self.ifs[band]
         cmd = ''
-        cmd += 'wl -i ' + ifname + ' down;'
-        cmd += 'wl -i ' + ifname + ' ap 0;'
-        cmd += 'wl -i ' + ifname + ' up;'
+        cmd += wl_cmd(ifname, 'down')
+        cmd += wl_cmd(ifname, 'ap 0')
+        cmd += wl_cmd(ifname, 'up')
         self.run_command(cmd)
 
     def set_ssid(self, ssid, band=5):
-        ifname = self.band_2_ifname(band)
+        ifname = self.ifs[band]
         cmd = ''
-        cmd += 'wl -i ' + ifname + ' down;'
-        cmd += 'wl -i ' + ifname + ' ssid ' + ssid + ';'
-        cmd += 'wl -i ' + ifname + ' up;'
+        cmd += wl_cmd(ifname, 'down')
+        cmd += wl_cmd(ifname, 'ssid ' + ssid)
+        cmd += wl_cmd(ifname, 'up')
         self.run_command(cmd)
 
     def enable_radio(self, band=5):
-        ifname = self.band_2_ifname(band)
-        self.run_command('wl -i ' + ifname + ' up')
+        ifname = self.ifs[band]
+        cmd = wl_cmd(ifname, 'up')
+        self.run_command(cmd)
         self.radio_enabled = True
 
     def disable_radio(self, band=5):
-        ifname = self.band_2_ifname(band)
-        self.run_command('wl -i ' + ifname + ' down')
+        ifname = self.ifs[band]
+        cmd = wl_cmd(ifname, 'down')
+        self.run_command(cmd)
         self.radio_enabled = False
 
     def set_wds_link(self, dest_mac, band=5):
-        ifname = self.band_2_ifname(band)
-        self.run_command('wl -i ' + ifname + ' wds ' + dest_mac)
+        ifname = self.ifs[band]
+        cmd = wl_cmd(ifname, 'wds ' + dest_mac)
+        self.run_command(cmd)
+
+    def learn_mac(self, band=5):
+        ifname = self.ifs[band]
+        cmd = 'ifconfig ' + ifname + ' | grep -o ..:..:..:..:..:..'
+        lines = self.run_command(cmd)
+        mac = lines[-2].strip()
+        return mac
 
 
 def main():
-    asd = ApNode('192.168.2.5', '', 2)
-    asd.set_ssid('potato', band=2)
-    asd.set_ssid('tomato', band=2)
+    asd = ApNode('192.168.2.5', '', 2, if2g='wl0', if5g='wl0')
+    #asd.set_ssid('potato', band=2)
+    #asd.set_ssid('kereviz', band=2)
+    print asd.macs
     pass
 
 if __name__ == '__main__':

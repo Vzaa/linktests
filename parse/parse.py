6e3db4b630ad7af8dd4bd227da81d51449839c2d
Vzaa <Vzaa@users.noreply.github.com>
2015-03-19 12:32:10 +0200

    add plotting with matplotlib

--- parse/parse.py
+++ parse/parse.py
@@ -3,6 +3,9 @@ import os
 import re
 import json
 
+import matplotlib.pyplot as plt
+import numpy as np
+
 
 def parse_iperf(lines):
     "parse iperf output"
@@ -78,11 +81,10 @@ def gen_test_dict(dirname, filename):
     return test
 
 
-def filter_tests(tests, testid, band, bw, channel, chain_src, chain_dst, t_type):
+def filter_tests(tests, testid, band, bw, chain_src, chain_dst, t_type):
     "filter tests"
     filtered = [test for test in tests
-                if test['channel'] == channel and
-                test['testid'] == testid and
+                if test['testid'] == testid and
                 test['chain_src'] == chain_src and
                 test['chain_dst'] == chain_dst and
                 test['type'] == t_type and
@@ -108,46 +110,103 @@ def get_unique_testids(tests):
         testids.add(test['testid'])
     return list(testids)
 
+def get_unique_nodes(tests):
+    "return a list of unique nodes in a tests list"
+    nodes = set()
+    for test in tests:
+        nodes.add(test['src'])
+        nodes.add(test['dest'])
+    return list(nodes)
+
+
+def plot_heatmap(mat, filename, vmin=0, vmax=600):
+    vals = []
+    column_names = range(1, 8)
+    for i, row in enumerate(mat):
+        for j, col in enumerate(row):
+            plt.text(j - 0.15, i + 0.1, str(int(round(col))), fontsize=14, color='black')
+            vals.append(col)
+    plt.imshow(mat, origin='upper', interpolation='nearest', cmap='YlGn', vmin=vmin, vmax=vmax)
+    plt.colorbar()
+    ticksx = np.arange(0.0, 7, 1)
+    plt.xticks(ticksx, column_names)
+    plt.yticks(ticksx, column_names)
+    plt.ylabel('Source Node')
+    plt.xlabel('Destination Node')
+    #plt.show()
+    #exit()
+
+    plt.savefig('./plots/%s.png' % filename)
+
+def get_dat_matrix(tests, testid, nodes, band, bw, chain_src, chain_dst):
+    mat = []
+    table = filter_tests(tests, testid, band, bw, chain_src, chain_dst, 'serv')
+    for node_a in nodes:
+        row = []
+        for node_b in nodes:
+            dat = [test for test in table if test['src'] == node_a and test['dest'] == node_b]
+            if len(dat) == 0:
+                row.append(0)
+            else:
+                row.append(dat[0]['dat'])
+        mat.append(row)
+    return mat
+
+def get_rssi_mat(tests, testid, nodes, band, bw, chain_src, chain_dst):
+    mat = []
+    table = filter_tests(tests, testid, band, bw, chain_src, chain_dst, 'dev')
+    for node_a in nodes:
+        row = []
+        for node_b in nodes:
+            dat = [test for test in table if test['src'] == node_a and test['dest'] == node_b]
+            if len(dat) == 0:
+                row.append(0)
+            else:
+                row.append(dat[0]['rssi'])
+        mat.append(row)
+    return mat
+
 
 def main():
     "main"
-    #tests = parse_dir('../logs/')
     tests = parse_dir('./5th_floor_logs/')
     testids = get_unique_testids(tests)
 
+    ch_5g = [('3x3', '3x3'), ('2x2', '2x2'), ('1x1', '1x1'), ('3x3', '2x2'), ('3x3', '1x1'), ('2x2', '1x1')]
+    ch_2g = [('2x2', '2x2'), ('1x1', '1x1'), ('2x2', '1x1')]
+
+    bw_2g = ['bw20', 'bw40']
+    bw_5g = ['bw20', 'bw40', 'bw80']
+
+
     for testid in testids:
-        #dat = filter_tests(tests, testid, '5g', 'bw80', 'ch100', '3x3', '1x1')
+        nodes = get_unique_nodes(tests) #TODO: check testid
+        for bw in bw_5g:
+            for (chain_src, chain_dst) in ch_5g:
+                mat = get_dat_matrix(tests, testid, nodes, '5g', bw, chain_src, chain_dst)
+                filename = "%s_%s_%s_%s" % ('5g', chain_src, chain_dst, bw)
+                plt.clf()
+                plot_heatmap(mat, filename)
+                mat = get_rssi_mat(tests, testid, nodes, '5g', bw, chain_src, chain_dst)
+                filename = "%s_%s_%s_%s_rssi" % ('5g', chain_src, chain_dst, bw)
+                plt.clf()
+                plot_heatmap(mat, filename, -100, 0)
+
+        for bw in bw_2g:
+            for (chain_src, chain_dst) in ch_2g:
+                mat = get_dat_matrix(tests, testid, nodes, '2g', bw, chain_src, chain_dst)
+                filename = "%s_%s_%s_%s" % ('2g', chain_src, chain_dst, bw)
+                plt.clf()
+                plot_heatmap(mat, filename)
+                mat = get_rssi_mat(tests, testid, nodes, '2g', bw, chain_src, chain_dst)
+                filename = "%s_%s_%s_%s_rssi" % ('2g', chain_src, chain_dst, bw)
+                plt.clf()
+                plot_heatmap(mat, filename, -100, 0)
+
         test_dat = [test for test in tests
                 if test['testid'] == testid and (test['type'] == 'serv' or test['type'] == 'dev')]
         with open('%s.json' % testid, 'w') as fpd:
             json.dump(test_dat, fpd, indent=4)
-
-
-
-
-
-    quit()
-    #ch_5g= [('3x3', '3x3'), ('2x2', '2x2'), ('1x1', '1x1'), ('3x3', '2x2'), ('3x3', '1x1'), ('2x2', '1x1')]
-    #ch_2g= [('2x2', '2x2'), ('1x1', '1x1'), ('2x2', '1x1')]
-    #dump rssis
-    ch_5g= [('3x3', '3x3')]
-    ch_2g= [('2x2', '2x2')]
-
-    #for bw in ['bw20', 'bw40', 'bw80']:
-    for bw in ['bw80']:
-        for cha, chb in ch_5g:
-            dat = filter_tests(tests, testid, '5g', bw, 'ch100', cha, chb, 'dev')
-            for item in dat:
-                #print item['rssi'], item['src'], item['dest'], item['chain_src'], item['chain_dst'], item['bw']
-                print item['rssi'], item['src'], item['dest'], item['band']
-
-    #for bw in ['bw20', 'bw40']:
-    for bw in ['bw40']:
-        for cha, chb in ch_2g:
-            dat = filter_tests(tests, testid, '2g', bw, 'ch6', cha, chb, 'dev')
-            for item in dat:
-                #print item['rssi'], item['src'], item['dest'], item['chain_src'], item['chain_dst'], item['bw']
-                print item['rssi'], item['src'], item['dest'], item['band']
 
 
 if __name__ == '__main__':
